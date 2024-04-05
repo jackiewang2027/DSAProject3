00cb8cfc430cd662017a9303dcc8f2f54277b0db
Jackie <jackiewang@ufl.edu>
2024-04-05 09:54:54 -0400

    Reads csv file

--- star.cpp
+++ star.cpp
@@ -43,42 +43,6 @@ star::star(std::string id , std::string hipID , std::string  hdID , std::string 
     this->varMax = varMax;
 }
 
-
-/*std::fstream fin;
-    fin.open("Star Data.csv", std::ios::in);
-
-    std::vector<std::string> row;
-    std::string temp;
-    int iter = 0;
-    while (fin >> temp) {
-
-        row.clear();
-
-        // read an entire row and
-        // store it in a string variable 'line'
-        //getline(fin, line);
-
-        // used for breaking words
-        std::stringstream lineStream(temp);
-        std::string attribute;
-        // read every column data of a row and
-        // store it in a string variable, 'word'
-        while (getline(lineStream, attribute, ',')) {
-                // add all the column data
-                // of a row to a vector
-                row.push_back(attribute);
-            }
-
-        if(!row.empty()){
-            if(row.size() != 37){
-                row.push_back("");
-            }
-            this->stars.push_back(star(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27], row[28], row[29], row[30], row[31], row[32], row[33], row[34], row[35], row[36]));
-        }
-        iter++;
-    }
-    this->stars.erase(this->stars.begin());
-    return this->stars;*/
 std::vector<star> star::readStarFile() {
     std::fstream fin;
     fin.open("Star Data.csv", std::ios::in);
