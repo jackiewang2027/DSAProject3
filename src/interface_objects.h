c164a2efc98a96fda0bb26640903fcef64dd8d92
alanzhangithub <117048020+alanzhangithub@users.noreply.github.com>
2024-04-17 23:13:36 -0400

    cleaned up files

--- src/interface_objects.h
+++ src/interface_objects.h
@@ -17,4 +17,35 @@ using namespace std;
 //Soundtrack (SMG OST: Star Festival)
 //https://www.youtube.com/watch?v=XwM0lPS07Oc
 
+
+void setupWelcomeText(sf::Text& text, sf::Font& font);
+void setupStarJumpText(sf::Text& text, sf::Font& font);
+void drawText(sf::RenderWindow& window, sf::Text& text);
+
+void setupWelcomeText(sf::Text& text, sf::Font& font) {
+    text.setFont(font);
+    text.setString("Welcome To The");
+    text.setCharacterSize(42);
+    text.setFillColor(sf::Color::White);
+    text.setStyle(sf::Text::Bold);
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.width / 2, textRect.height / 2);
+    text.setPosition(400, 240);
+}
+
+void setupStarJumpText(sf::Text& text, sf::Font& font) {
+    text.setFont(font);
+    text.setString("Star Jump Visualizer");
+    text.setCharacterSize(46);
+    text.setFillColor(sf::Color::White);
+    text.setStyle(sf::Text::Bold);
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.width / 2, textRect.height / 2);
+    text.setPosition(400, 280);
+}
+
+void drawText(sf::RenderWindow& window, sf::Text& text) {
+    window.draw(text);
+}
 #endif //CMAKESFMLPROJECT_INTERFACE_OBJECTS_H
+
